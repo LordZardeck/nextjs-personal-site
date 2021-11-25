55bc5f6d299d49d6f4caf62e9689caccfb6b360e
Sean Templeton <sean@templeton.io>
2021-11-24 22:45:54 -0800

    Updated pages to support metadata

--- components/modules/page-module.js
+++ components/modules/page-module.js
@@ -1,9 +1,32 @@
 import ModuleDelegator from "../common/module-delegator";
+import Head from "next/head";
+import {useRouter} from "next/router";
 
-export default function PageModule({body}) {
+export default function PageModule({body, metadata}) {
+    const router = useRouter();
+
     return (
-        <div className="page">
-            <ModuleDelegator modules={body} />
-        </div>
+        <>
+            <Head>
+                <meta name="author" content="Sean Templeton"/>
+                <meta property="og:locale" content="en"/>
+                <meta property="og:site_name" content="Sean Templeton: Web Developer"/>
+                <link rel="canonical" href={`https://templeton.io/${router.asPath}`}/>
+                <meta property="og:url" content={`https://templeton.io/${router.asPath}`}/>
+                <meta property="og:type" content="article"/>
+                {
+                    metadata &&
+                    <>
+                        <meta name="description" content={metadata.description}/>
+                        <meta property="og:description" content={metadata.description}/>
+                        <meta property="og:title" content={metadata.title}/>
+                        <title>{metadata.title}</title>
+                    </>
+                }
+            </Head>
+            <div className="page">
+                <ModuleDelegator modules={body}/>
+            </div>
+        </>
     );
 }
