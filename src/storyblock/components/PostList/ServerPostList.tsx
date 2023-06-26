3293253060cb4d7c390feaa8bfdd8d5bd17a16aa
Sean Templeton <sean@templeton.io>
2023-06-25 22:18:28 -0500

    Remove unused import

--- src/storyblock/components/PostList/ServerPostList.tsx
+++ src/storyblock/components/PostList/ServerPostList.tsx
@@ -1,5 +1,4 @@
 import type { BlokProps } from '@/storyblock/components/types'
-import { loader } from './loader'
 import { ClientPostList } from './ClientPostList'
 import type { PostList } from './ClientPostList'
 
