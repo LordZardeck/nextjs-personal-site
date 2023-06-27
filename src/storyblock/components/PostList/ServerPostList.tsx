6a67002980560d06df45eb4bb3a957aa48b5ba1d
Sean Templeton <sean@templeton.io>
2023-06-27 00:17:34 -0500

    Revert changes to server post list, still can't get it to work
    Most likely due to the fact that components specified in init storybook are client always

--- src/storyblock/components/PostList/ServerPostList.tsx
+++ src/storyblock/components/PostList/ServerPostList.tsx
@@ -1,28 +1,7 @@
-import type { BlokProps, Component } from '@/storyblock/components/types'
-import { loader } from './loader'
-import { PostListItem } from './PostListItem'
-
-type PostList = Component<
-  { count: string; endDate: string; startDate: string },
-  'postList'
->
+import type { BlokProps } from '@/storyblock/components/types'
+import { ClientPostList } from './ClientPostList'
+import type { PostList } from './ClientPostList'
 
 export async function PostList({ blok }: BlokProps<PostList>) {
-  const { startDate, endDate, count } = blok
-  const posts = await loader(Number(count), startDate, endDate)
-
-  return (
-    <>
-      {posts.map(
-        ({
-          name: title,
-          first_published_at: postedAt,
-          content: { summary },
-          slug,
-        }) => (
-          <PostListItem key={slug} {...{ title, summary, postedAt, slug }} />
-        ),
-      )}
-    </>
-  )
+  return <ClientPostList blok={blok} preloaded={[]} />
 }
